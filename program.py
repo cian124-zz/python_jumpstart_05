bbf326e260413b9222b6d5e8400d0bd58509174d
cian124 <cian12493@gmail.com>
2018-06-14 17:13:59 +0900

    User enters country name now Converts Fahrenheit to Celsius

--- program.py
+++ program.py
@@ -15,6 +15,10 @@ def draw_header():
 
 def get_location():
     country = input('Please enter a country code: ')
+    country = get_country_code(country)
+    while country is None:
+        country = input('Invalid country, please try again: ')
+        country = get_country_code(country)
     if country == 'us':
         state = input('Please enter the state code: ')
     else:
@@ -23,6 +27,16 @@ def get_location():
     city = city.replace(' ', '-').lower()
     loc = Location(country, state, city)
     return loc
+
+
+def get_country_code(country):
+    country = country.capitalize()
+    req = requests.get('https://www.worldatlas.com/aatlas/ctycodes.htm')
+    soup = bs4.BeautifulSoup(req.text, "html.parser")
+    code = soup.find(class_='misc-content').find_next('td', string=country)
+    if code:
+        code = code.find_next().get_text().lower()
+    return code
 
 
 def get_weather(loc):
@@ -36,6 +50,11 @@ def get_weather(loc):
     temp = soup.find(class_='wu-unit-temperature').find(class_='wu-value').get_text()
     scale = soup.find(class_='wu-unit-temperature').find(class_='wu-label').get_text()
     scale = cleanup_text(scale)
+    if scale == 'F':
+        temp = int(temp)
+        temp = round((temp - 32) * 0.5556, 1)
+        temp = str(temp)
+        scale = 'C'
     condition = soup.find(class_='condition-icon').get_text()
     condition = cleanup_text(condition)
     city = soup.find(class_='region-content-header').find('h1').get_text()
